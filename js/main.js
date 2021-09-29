0ad02839e538fc9939820f922f93d0dd9a658bff
Siddharth Vivek <gargantuan5k@outlook.com>
2021-09-29 09:53:52 +0530

    Fix issue #2 Issue: Reloading page sometimes makes homepage card elements stack on each other Remarks: Issue caused in Firefox, Opera browsers Fix: Wrap Masonry and Swiper object creation (/js/main.js) in masonryInit() function, call masonryInit() in 'onload' property of window object Issue also found in https://github.com/desandro/masonry/issues/993

--- js/main.js
+++ js/main.js
@@ -5,48 +5,52 @@ toggle.addEventListener('click', function() {
     collapse.forEach(col => col.classList.toggle("collapse-toggle"))
 })
 
-//with masonry
-new Masonry("#posts .grid", {
-    itemSelector: '.grid-item',
-    gutter: 100
-});
+// Wrapped Masonry and Swiper object creation inside a function and call it in window.onload property (Fixes issue #2)
+window.onload = function() { masonryInit() }
 
-// swiper library initialization
-new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 5,
-    autoplay: {
-        delay: 3000
-    },
-    //responsive breakpoints
-    breakpoints: {
-        '@0': {
-            slidesPerView: 2
+function masonryInit() {
+    new Masonry("#posts .grid", {
+        itemSelector: '.grid-item',
+        gutter: 100
+    });
+    
+    // swiper library initialization
+    new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 5,
+        autoplay: {
+            delay: 3000
         },
-        //>888px
-        '@1.00': {
-            slidesPerView: 3
-        },
-        //>1110px
-        '@1.25': {
-            slidesPerView: 4
-        },
-        //>1330px
-        '@1.50': {
-            slidesPerView: 5
+        //responsive breakpoints
+        breakpoints: {
+            '@0': {
+                slidesPerView: 2
+            },
+            //>888px
+            '@1.00': {
+                slidesPerView: 3
+            },
+            //>1110px
+            '@1.25': {
+                slidesPerView: 4
+            },
+            //>1330px
+            '@1.50': {
+                slidesPerView: 5
+            }
         }
-    }
-})
+    })
+}
 
 //Sticky Navigation Bar
-window.onscroll = function() { myFunction() };
+window.onscroll = function() { createStickyHeader() };
 
 let navbar = document.getElementById("header");
 let sticky = navbar.offsetTop;
 
 //sticky function
-function myFunction() {
+function createStickyHeader() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
     } else {
